6969740e6cdbc8d9ea0f07e93382eac59f62dc51
Kyle Mosier <mosierkyle@gmail.com>
2023-08-26 22:46:25 -0700

    Create and style menu page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { loadHeader } from './header'
 import { loadInitialPage } from './initial-page'
+import { loadMenuPage } from './menu';
 import "./main.css";
 
 loadHeader();
 
-loadInitialPage();
+// loadInitialPage()
+loadMenuPage()
+
+
