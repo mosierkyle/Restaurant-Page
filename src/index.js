5022f066a9e8e2f8e5d5ef25c38017cd6eec8822
Kyle Mosier <mosierkyle@gmail.com>
2023-08-27 10:09:52 -0700

    Create and style contact page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import { loadHeader } from './header'
 import { loadInitialPage } from './initial-page'
 import { loadMenuPage } from './menu';
 import "./main.css";
+import { loadContactPage } from './contact';
 
 loadHeader();
 
+loadContactPage()
 // loadInitialPage()
-loadMenuPage()
+// loadMenuPage()
 
 
